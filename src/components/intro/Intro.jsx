4ac75bf8fcc6fbebf45794749981636c66141812
AnshumanSAMA <a.mander@uqconnect.edu.au>
2022-06-03 14:40:10 +1000

    Responsive WIP

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,6 +2,27 @@ import { React, useEffect, useRef } from "react";
 import { init } from "ityped";
 import "./Intro.scss";
 
+const Contacts = () => {
+  return (
+    <div id="contacts">
+      <div className="contact-icon">
+        <a href="https://github.com/AnshSAMA" target="_blank" rel="noopener noreferrer">
+          <img src="images/logos/github-logo.svg" alt="Github Link" />
+        </a>
+      </div>
+      <div className="contact-icon">
+        <a href="https://www.linkedin.com/in/anshuman-mander-7b3137193/" target="_blank" rel="noopener noreferrer">
+          <img src="images/logos/linkedin-logo.svg" alt="LinkedIn Link" />
+        </a>
+      </div>
+      <div className="contact-icon">
+        <a href="mander.anshuman@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img src="images/logos/email-logo.svg" alt="Email Link" />
+        </a>
+      </div>
+    </div>
+  );
+};
 export default function Intro() {
   const textRef = useRef();
 
@@ -19,18 +40,20 @@ export default function Intro() {
 
   //return
   return (
-    <div id="intro">
-      <div id="left">
-        <div>
-          <p>Hello,</p>
-          <p id="name">I'm Anshuman</p>
-          <p>I am a developer who loves</p>
-          <span ref={textRef}></span>
-          <div id="contacts"></div>
+    <div id="page">
+      <div id="intro">
+        <div id="left">
+          <div>
+            <p>Hello,</p>
+            <p id="name">I'm Anshuman</p>
+            <p>I am a developer who loves</p>
+            <span ref={textRef}></span>
+            <Contacts />
+          </div>
         </div>
-      </div>
-      <div id="right">
-        <img src="images/intro-self.jpg" alt="My Self Portrait" />
+        <div id="right">
+          <img src="images/intro-self.jpg" alt="My Self Portrait" />
+        </div>
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,25 +1,58 @@
 import "./Topbar.scss";
-
-// import MenuIcon from "@mui/icons-material/Menu";
-// import CloseIcon from "@mui/icons-material/Close";
-
-// import { useState } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Topbar() {
-  //   const [topbarActive, setTopbarActive] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className={`topbar`} id="topbar">
-      <div className="wrapper">
-        <div className="left">
+    <div id="topbar">
+      <div id="wrapper">
+        <div id="left">
           <a href="#intro" className="logo">
             amander.
           </a>
         </div>
 
-        <div className="right hamburger">
-          <a href="#skills">skills</a>
-          <a href="#works">works</a>
-          <a href="#resume">resume</a>
+        <div id="right">
+          <div id="hamburger-menu">
+            <Button onClick={openMenu}>{anchorEl === null ? <MenuIcon className="menu-icon" /> : <CloseIcon className="menu-icon" />}</Button>
+            <Menu anchorEl={anchorEl} open={open} onClose={closeMenu}>
+              <MenuItem onClick={closeMenu}>
+                <a className="right-navs" href="#skills">
+                  skills
+                </a>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <a className="right-navs" href="#works">
+                  works
+                </a>
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                <a className="right-navs" href="./assets/Resume - Anshuman Mander.pdf" download>
+                  resume
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
+          <>
+            <a href="#skills">skills</a>
+            <a href="#works">works</a>
+            <a href="./assets/Resume - Anshuman Mander.pdf" download>
+              resume
+            </a>
+          </>
         </div>
       </div>
     </div>
